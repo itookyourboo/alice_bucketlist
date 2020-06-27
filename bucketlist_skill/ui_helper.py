261830b77d9ed016c8786fd1b3a8ba23067699a7
itookyourboo <Wignorbo@yandex.ru>
2020-06-27 11:16:36 +0300

    default_buttons, save_res and normalize_tts

---
+++ bucketlist_skill/ui_helper.py
@@ -0,0 +1,34 @@
+from base_skill.skill import button
+from .strings import btn, BUTTONS
+
+
+def default_buttons(func):
+    def wrapper(req, res, session):
+        func(req, res, session)
+        if len(res.buttons) == 0:
+            res.buttons = [button(x) for x in btn(BUTTONS.get(session['state'], []))]
+        else:
+            for x in btn(BUTTONS.get(session['state'], [])):
+                res.buttons.append(button(x))
+
+    return wrapper
+
+
+def save_res(func):
+    def wrapper(req, res, session):
+        func(req, res, session)
+        session['last_text'] = None
+        session['last_tts'] = None
+
+        session['last_text'] = res.text
+        session['last_tts'] = res.tts
+
+    return wrapper
+
+
+def normalize_tts(func):
+    def wrapper(req, res, session):
+        func(req, res, session)
+        res.tts = res.tts.replace('тест', 'т+эст').replace('Гааааааляяяяяяяяя', 'Га а а аля')
+
+    return wrapper
